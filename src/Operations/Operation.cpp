5bee2d184e749a2cf841380693f9daf99b535ee3
lgarczyn <lgarczyn@student.42.fr>
2019-04-13 09:05:25 +0200

    cleaned up operation call pointer

--- src/Operations/Operation.cpp
+++ src/Operations/Operation.cpp
@@ -64,22 +64,14 @@ bool Operation::run(Stack &stack)
 	if ((size_t)info.required_operands > stack.size())
 		throw StackTooSmallException(info.name);
 
-	const IOperand *highest = NULL;
-	const IOperand * lowest = NULL;
-
-	if (info.required_operands >= 1) {
-		highest = stack.back();
-	}
-	if (info.required_operands >= 2) {
-		lowest = stack.at(stack.size() - 2);
-	}
-
 	try
 	{
-		(this->*(info.method))(stack, highest, lowest);
+		(this->*(info.method))(stack);
 	}
 	catch (SafeIntException &e)
 	{
+		const IOperand *highest = stack.back();
+		const IOperand *lowest = stack.at(stack.size() - 2);
 		throw OverflowException(lowest->toString(), info.representation, highest->toString());
 	}
 	
@@ -87,28 +79,30 @@ bool Operation::run(Stack &stack)
 }
 
 // Pushes the value at the top of the stack.
-void Operation::op_push(Stack &stack, const IOperand *, const IOperand *)
+void Operation::op_push(Stack &stack)
 {
 	stack.push_back(_operand);
 	_operand = NULL;
 }
 
 // Unstacks the value from the top of the stack.  If the stack is empty, theprogram execution must stop with an error.
-void Operation::op_pop(Stack &stack, const IOperand *highest, const IOperand *)
+void Operation::op_pop(Stack &stack)
 {
+	const IOperand *highest = stack.back();
 	stack.pop_back();
 	delete highest;
 }
 
 // Pushes the value at the top of the stack.
-void Operation::op_dup(Stack &stack, const IOperand *highest, const IOperand *)
+void Operation::op_dup(Stack &stack)
 {
+	const IOperand *highest = stack.back();
 	stack.push_back(highest->clone());
 	_operand = NULL;
 }
 
 // Displays each value of the stack, from the most recent one to the oldestone WITHOUT CHANGING the stack. Each value is separated from the next oneby a newline.
-void Operation::op_dump(Stack &stack, const IOperand *, const IOperand *)
+void Operation::op_dump(Stack &stack)
 {
 	for (auto it = stack.rbegin(); it != stack.rend(); it++)
 	{
@@ -117,8 +111,9 @@ void Operation::op_dump(Stack &stack, const IOperand *, const IOperand *)
 }
 
 // Asserts that the value at the top of the stack is equal to the one passedas parameter for this instruction. If it is not the case, the program execution muststop with an error. The valuevhas the same form that those passed as parametersto the instructionpush.
-void Operation::op_assert(Stack &, const IOperand *highest, const IOperand *)
+void Operation::op_assert(Stack &stack)
 {
+	const IOperand *highest = stack.back();
 	if (*highest != *this->_operand)
 	{
 		throw FailedAssertException(this->_operand->toString(), highest->toString());
@@ -126,8 +121,11 @@ void Operation::op_assert(Stack &, const IOperand *highest, const IOperand *)
 }
 
 // Unstacks the first two values on the stack, adds them together and stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.
-void Operation::op_add(Stack &stack, const IOperand *highest, const IOperand *lowest)
+void Operation::op_add(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+	const IOperand *lowest = stack.at(stack.size() - 2);
+
 	auto res = *lowest + *highest;
 	stack.pop_back();
 	stack.pop_back();
@@ -137,8 +135,11 @@ void Operation::op_add(Stack &stack, const IOperand *highest, const IOperand *lo
 }
 
 // Unstacks the first two values on the stack, subtracts them, then stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.
-void Operation::op_sub(Stack &stack, const IOperand *highest, const IOperand *lowest)
+void Operation::op_sub(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+	const IOperand *lowest = stack.at(stack.size() - 2);
+
 	auto res = *lowest - *highest;
 	stack.pop_back();
 	stack.pop_back();
@@ -148,8 +149,11 @@ void Operation::op_sub(Stack &stack, const IOperand *highest, const IOperand *lo
 }
 
 // Unstacks the first two values on the stack, multiplies them, then stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.4
-void Operation::op_mul(Stack &stack, const IOperand *highest, const IOperand *lowest)
+void Operation::op_mul(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+	const IOperand *lowest = stack.at(stack.size() - 2);
+
 	auto res = *lowest * *highest;
 	stack.pop_back();
 	stack.pop_back();
@@ -159,8 +163,11 @@ void Operation::op_mul(Stack &stack, const IOperand *highest, const IOperand *lo
 }
 
 // Unstacks the first two values on the stack, divides them, then stacks the result.If the number of values on the stack is strictly inferior to 2, the program executionmust stop with an error. Moreover, if the divisor is equal to 0, the program executionmust stop with an error too. Chatting about floating point values is relevant a thispoint. If you don’t understand why, some will understand. The linked question isan open one, there’s no definitive answer.
-void Operation::op_div(Stack &stack, const IOperand *highest, const IOperand *lowest)
+void Operation::op_div(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+	const IOperand *lowest = stack.at(stack.size() - 2);
+
 	if (highest->isZero())
 		throw DivideByZeroException();
 	auto res = *lowest / *highest;
@@ -172,8 +179,11 @@ void Operation::op_div(Stack &stack, const IOperand *highest, const IOperand *lo
 }
 
 // Unstacks the first two values on the stack, calculates the modulus, thenstacks the result. If the number of values on the stack is strictly inferior to 2, theprogram execution must stop with an error. Moreover, if the divisor is equal to 0,the program execution must stop with an error too. Same note as above about fpvalues.
-void Operation::op_mod(Stack &stack, const IOperand *highest, const IOperand *lowest)
+void Operation::op_mod(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+	const IOperand *lowest = stack.at(stack.size() - 2);
+
 	if (highest->isZero())
 		throw ModuloByZeroException();
 	auto res = *lowest % *highest;
@@ -185,12 +195,14 @@ void Operation::op_mod(Stack &stack, const IOperand *highest, const IOperand *lo
 }
 
 // Asserts that the value at the top of the stack is an 8-bit integer. (If not,see the instructionassert), then interprets it as an ASCII value and displays thecorresponding character on the standard output.
-void Operation::op_print(Stack &, const IOperand *highest, const IOperand *)
+void Operation::op_print(Stack &stack)
 {
+	const IOperand *highest = stack.back();
+
 	if (highest->getType() != e_ty_i8)
 		throw ForbiddenPrintException(highest->getTypeName());
 	std::cout << static_cast<int8_t>(highest->asI8()) << std::endl;
 }
 
 // Terminate the execution of the current program. If this instruction does notappears while all others instruction has been processed, the execution must stop with an error
-void Operation::op_exit(Stack &, const IOperand *, const IOperand *) {}
+void Operation::op_exit(Stack &){}
--- src/Operations/Operation.hpp
+++ src/Operations/Operation.hpp
@@ -26,7 +26,7 @@ class Operation;
 
 typedef std::vector<const IOperand *> Stack;
 
-typedef void (Operation::*OperationMethod)(Stack &stack, const IOperand *, const IOperand *);
+typedef void (Operation::*OperationMethod)(Stack &stack);
 
 struct OperationInfo
 {
@@ -68,28 +68,28 @@ class Operation
 
 	bool run(Stack &stack);
 	// Pushes the value at the top of the stack.
-	void op_push(Stack &stack, const IOperand *, const IOperand *);
+	void op_push(Stack &stack);
 	// Unstacks the value from the top of the stack.  If the stack is empty, theprogram execution must stop with an error.
-	void op_pop(Stack &stack, const IOperand *, const IOperand *);
+	void op_pop(Stack &stack);
 	// Duplicates the highest element of the stack, and pushes it to the stack
-	void op_dup(Stack &stack, const IOperand *, const IOperand *);
+	void op_dup(Stack &stack);
 	// Displays each value of the stack, from the most recent one to the oldestone WITHOUT CHANGING the stack. Each value is separated from the next oneby a newline.
-	void op_dump(Stack &stack, const IOperand *, const IOperand *);
+	void op_dump(Stack &stack);
 
 	// Asserts that the value at the top of the stack is equal to the one passedas parameter for this instruction. If it is not the case, the program execution muststop with an error. The valuevhas the same form that those passed as parametersto the instructionpush.
-	void op_assert(Stack &stack, const IOperand *, const IOperand *);
+	void op_assert(Stack &stack);
 	// Unstacks the first two values on the stack, adds them together and stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.
-	void op_add(Stack &stack, const IOperand *, const IOperand *);
+	void op_add(Stack &stack);
 	// Unstacks the first two values on the stack, subtracts them, then stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.
-	void op_sub(Stack &stack, const IOperand *, const IOperand *);
+	void op_sub(Stack &stack);
 	// Unstacks the first two values on the stack, multiplies them, then stacks theresult. If the number of values on the stack is strictly inferior to 2, the programexecution must stop with an error.4
-	void op_mul(Stack &stack, const IOperand *, const IOperand *);
+	void op_mul(Stack &stack);
 	// Unstacks the first two values on the stack, divides them, then stacks the result.If the number of values on the stack is strictly inferior to 2, the program executionmust stop with an error. Moreover, if the divisor is equal to 0, the program executionmust stop with an error too. Chatting about floating point values is relevant a thispoint. If you don’t understand why, some will understand. The linked question isan open one, there’s no definitive answer.
-	void op_div(Stack &stack, const IOperand *, const IOperand *);
+	void op_div(Stack &stack);
 	// Unstacks the first two values on the stack, calculates the modulus, thenstacks the result. If the number of values on the stack is strictly inferior to 2, theprogram execution must stop with an error. Moreover, if the divisor is equal to 0,the program execution must stop with an error too. Same note as above about fpvalues.
-	void op_mod(Stack &stack, const IOperand *, const IOperand *);
+	void op_mod(Stack &stack);
 	// Asserts that the value at the top of the stack is an 8-bit integer. (If not,see the instructionassert), then interprets it as an ASCII value and displays thecorresponding character on the standard output.
-	void op_print(Stack &stack, const IOperand *, const IOperand *);
+	void op_print(Stack &stack);
 	// Terminate the execution of the current program. If this instruction does notappears while all others instruction has been processed, the execution must stop with an error
-	void op_exit(Stack &stack, const IOperand *, const IOperand *);
+	void op_exit(Stack &stack);
 };
